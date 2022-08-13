9b7eb4cada2e5c8128f6c4ddcc8695f4ddf7e09f
Muhammad Arif Budiman <arifsama10@gmail.com>
2022-08-13 20:23:30 +0700

    Menambah task3

---
+++ task3/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func sayHello(name string) {
+	fmt.Println("Hello!!", name, "Saya Go-lang Bahasa Indonesia Sangat Menyenangkan")
+}
+
+func main() {
+
+	sayHello("kobar")
+	sayHello("arif")
+
+}
